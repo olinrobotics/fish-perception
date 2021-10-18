f5237dcfeb129606e903efd3eaccf9cab070cd6f
David on Jetson <dtarazi@olin.edu>
2021-10-18 16:21:47 -0400

    Squashed commits for camera driver
    added sensor_driver package
    
    untested driver code for camera
    
    forgot publish line
    
    debug cmakelists
    
    added fix
    
    removed install for now
    
    built package, ready to test on rosie
    
    added hot fix
    
    add compile options
    
    one more fix

--- jetson_setup/install_packages.sh
+++ jetson_setup/install_packages.sh
@@ -4,8 +4,15 @@ apt install python-pip -y
 apt install curl -y
 apt install wireshark -y
 
+# install ros packages
+apt install ros-melodic-cv-bridge
+apt install ros-melodic-sensor-msgs
+
+# create link for opencv
+ln -s /usr/include/opencv4/opencv2/ /usr/include/opencv4
+
 # gpio interface with python
-pip3 install --upgrade pip
-pip3 install Jetson.GPIO -y
+pip install --upgrade pip
+pip install Jetson.GPIO -y
 groupadd -f -r gpio
 usermod -a -G gpio $USER
